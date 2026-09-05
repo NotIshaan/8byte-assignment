fc002bbb60ff1f5227979346190f35cb758fd349
Ishaan Bhadrike <ishaanworks24@gmail.com>
2026-09-05 21:09:36 +0400

    fix desired ec2 asg capacity

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -93,8 +93,8 @@ resource "aws_autoscaling_group" "app" {
   vpc_zone_identifier = aws_subnet.private[*].id
   target_group_arns   = [aws_lb_target_group.app.arn]
   min_size            = 1
-  max_size            = 3
-  desired_capacity    = 2
+  max_size            = 2
+  desired_capacity    = 1
   health_check_type   = "ELB"
 
   launch_template {

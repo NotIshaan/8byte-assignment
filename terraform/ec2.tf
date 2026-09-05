958bdcb9da84eca7e90ea38eced6817102866e17
Ishaan Bhadrike <ishaanworks24@gmail.com>
2026-09-05 20:51:07 +0400

    fix trivy version, passed project_name to userdata shell script

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -77,6 +77,7 @@ resource "aws_launch_template" "app" {
     secret_arn = aws_secretsmanager_secret.db.arn
     region     = var.aws_region
     ecr_url    = aws_ecr_repository.app.repository_url
+    project_name= var.project_name
   }))
 
   tag_specifications {

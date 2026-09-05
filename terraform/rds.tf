374fc8451fac9fb5f9fc05f23312f356eb279f88
Ishaan Bhadrike <ishaanworks24@gmail.com>
2026-09-05 23:42:33 +0400

    fixing server.js code and fixing rds db version

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -10,7 +10,7 @@ resource "aws_db_subnet_group" "main" {
 resource "aws_db_instance" "main" {
   identifier        = "${var.project_name}-db"
   engine            = "postgres"
-  engine_version    = "16.4"
+  engine_version    = "16.9"
   instance_class    = var.db_instance_class
   allocated_storage = 20
 
